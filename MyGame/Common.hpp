15d4e7f24b152f5f32b267a94596890e7887e715
winter2521 <winter2521@outlook.jp>
2024-10-22 21:27:08 +0900

    ファイル分割・Common.hppの作成

--- MyGame/Common.hpp
+++ MyGame/Common.hpp
@@ -1,2 +1,16 @@
-﻿#include <Siv3D.hpp>
+﻿# pragma once
+#include <Siv3D.hpp>
 
+// 扱うシーンの名前を列挙
+enum class State
+{
+	Title,
+	Game,
+};
+
+// シーン間で共有するデータの構造体
+struct GameData
+{
+};
+
+using App = SceneManager<State, GameData>;
