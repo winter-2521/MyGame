5f9624d4c0e4b1ea62dca27a041a594bebba492d
winter2521 <winter2521@outlook.jp>
2024-10-27 17:40:51 +0900

    Title画面をサンプルコードから移植．いったん動作を確認したのでpush

--- MyGame/Common.hpp
+++ MyGame/Common.hpp
@@ -1,4 +1,4 @@
-﻿# pragma once
+﻿#pragma once
 #include <Siv3D.hpp>
 
 // 扱うシーンの名前を列挙
--- MyGame/Main.cpp
+++ MyGame/Main.cpp
@@ -1,10 +1,28 @@
-﻿# include "Common.hpp"
+﻿#include "Common.hpp"
+#include "Title.hpp"
 
 void Main()
 {
-	
-	while (System::Update())
-	{
+	// Escキーによるプログラム終了を無効化
+	System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+
+	// F1キーによるライセンス表示を無効化
+	LicenseManager::DisableDefaultTrigger();
+
+	FontAsset::Register(U"TitleFont", FontMethod::MSDF, 50, U"example/font/RocknRoll/RocknRollOne-Regular.ttf");
+	FontAsset(U"TitleFont").setBufferThickness(4);
+	FontAsset::Register(U"Menu", FontMethod::MSDF, 40, Typeface::Medium);
+	FontAsset::Register(U"Ranking", 40, Typeface::Heavy);
+	FontAsset::Register(U"GameScore", 30, Typeface::Light);
+	AudioAsset::Register(U"Brick", GMInstrument::Woodblock, PianoKey::C5, 0.2s, 0.1s);
+
+	App manager;
+	manager.add<Title>(State::Title);
+
+	while (System::Update()){
+		if (not manager.update()){
+			break;
+		}
 	}
 }
 
